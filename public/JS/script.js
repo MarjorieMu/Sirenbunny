885253f33ec9083cbba7e6479deb996a2d946743
MarjorieMu <88872190+MarjorieMu@users.noreply.github.com>
2022-11-03 02:27:20 -0600

    Add files via upload

---
+++ public/JS/script.js
@@ -0,0 +1,63 @@
+/* hover imagen */
+
+const imgs = document.querySelectorAll('.img-select a');
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+/* carrusel imagenes - descripcion  */
+function slideImage(){
+    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+window.addEventListener('resize', slideImage);
+
+
+/* Botones subir y bajar */
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
+
+
+
+
+/* card */
+
+var tl = new TimelineMax();
+
+$('.card-number').focus(function(){
+  console.log('card-number-focus works!');
+  tl.to('.credit-card-number-group', .1, { background: '#FFAC0D', 
+                                            opacity: '20%',
+                                          bottom: '58px', 
+                                          border:'2px solid #FFAC0D'
+                                          })
+    .to('.card-num', .1, { position: 'relative', top:'10px', width:'50px', left: '18px'})
+  
+});
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
